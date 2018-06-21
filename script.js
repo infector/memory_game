145f39bc37d5b1a7884df4bfe2b1cfe7b584f619
infector <piotr.babik@gmail.com>
2018-06-21 16:01:24 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+$(function () {
+
+
+    class Game {
+
+        constructor() {
+            this.board = document.querySelector(".game-board");
+            this.tiles = $(this.board).children();
+        }
+
+        resetGame() {
+
+        }
+
+        shuffleCards() {
+
+        }
+
+    }
+
+
+
+
+    let game = new Game();
+
+
+
+
+
+
+
+
+})
