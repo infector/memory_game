5d68db3f6eaaad16d19e22344613ae4a524d1ce0
infector <piotr.babik@gmail.com>
2018-06-26 16:07:01 -0700

    added difficulty levels option

--- script.js
+++ script.js
@@ -12,14 +12,15 @@ $(function () {
 
     class Game {
 
-        constructor(board, tilesDivs) {
-            this.board = board;
-            this.tilesDivs = Array.from(tilesDivs);
-            this.tileBack = $(".grid-item").css("background-color");
+        constructor() {
+            // this.board = board;
+            this.tilesDivs = [];
+            // this.tileBack = $(".grid-item").css("background-color");
             this.pickedPair = [];
             this.pairsClickedCounter = 0;
             this.pairsGuessedCounter = 0;
             this.selectable = true;
+            this.difficulty = "";
             this.images = {
                 img1: "img/PNG/2.png",
                 img2: "img/PNG/3.png",
@@ -28,46 +29,92 @@ $(function () {
                 img5: "img/PNG/9.png",
                 img6: "img/PNG/16.png",
                 img7: "img/PNG/17.png",
-                img8: "img/PNG/18.png"
+                img8: "img/PNG/18.png",
+                img9: "img/PNG/4.png",
+                img10: "img/PNG/5.png",
+                img11: "img/PNG/8.png",
+                img12: "img/PNG/10.png",
+                img13: "img/PNG/11.png",
+                img14: "img/PNG/12.png",
+                img15: "img/PNG/13.png",
+                img16: "img/PNG/14.png",
+                img17: "img/PNG/15.png",
+                img18: "img/PNG/19.png"
             };
 
         }
 
         newGame() {
 
-            let pickedPair = this.pickedPair;
-            let selectable = this.selectable;
+            $(".game-board").empty();
+            this.tilesDivs.length = 0;
+            $("select").prop("selectedIndex", -1);
+            this.createBoard();
+        }
+
+        createBoard() {
+
+            let that = this;
+
+            $("select").change(function () {
+
+                let element = '<div class="grid-item"></div>';
+                let createdDiv;
+                let difficulty = parseInt($("select").val());
+                let sideElemsCount = Math.sqrt(difficulty);
+                let sideElemSize = 80 / sideElemsCount + "vmin";
+                $(".game-board").css("grid-template-columns", `repeat(${sideElemsCount}, ${sideElemSize})`);
+                $(".game-board").css("grid-template-rows", `repeat(${sideElemsCount}, ${sideElemSize})`);
+
+                for (let i = 0; i < difficulty; i++) {
+                    $(".game-board").append(element);
+                    createdDiv = $(".game-board").children(".grid-item").eq(i);
+                    that.tilesDivs.push(createdDiv);
+                }
+
+                that.addGameMechanics();
+                that.shuffleCards();
+            });
+
+        }
+
+        addGameMechanics() {
+
+            // let pickedPair = this.pickedPair;
+            // let selectable = this.selectable;
+            let that = this;
 
             $(".grid-item").click(function () {
-
-                if (selectable == true) {
+                if (that.selectable === true) {
                     $(this).css("background-image", $(this).data("image"));
 
-                    pickedPair.push($(this));
+                    that.pickedPair.push($(this));
 
-                    if ($(pickedPair[0]).data("number") === $(this).data("number") && pickedPair.length === 2) {
-                        pickedPair.pop();
+                    if ($(that.pickedPair[0]).data("number") === $(this).data("number") && that.pickedPair.length === 2) {
+                        that.pickedPair.pop();
                     }
                 }
 
-                if (pickedPair.length === 2 && selectable == true && $(pickedPair[0]).data("image") !== $(pickedPair[1]).data("image")) {
+                if (that.pickedPair.length === 2 && that.selectable == true && $(that.pickedPair[0]).data("image") !== $(that.pickedPair[1]).data("image")) {
 
-                    selectable = false;
+                    that.selectable = false;
 
                     setTimeout(function () {
 
-                        $(pickedPair[0]).removeAttr("style");
-                        $(pickedPair[1]).removeAttr("style");
-                        pickedPair = [];
-                        selectable = true;
+                        $(that.pickedPair[0]).removeAttr("style");
+                        $(that.pickedPair[1]).removeAttr("style");
+                        that.pickedPair.length = 0;
+                        that.selectable = true;
 
-                    }, 250);
+                    }, 500);
 
-                } else if (pickedPair.length === 2 && $(pickedPair[0]).data("image") === $(pickedPair[1]).data("image")) {
+                    console.log(that.pairsClickedCounter++);
 
-                    $(pickedPair[0]).off("click");
-                    $(pickedPair[1]).off("click");
-                    pickedPair = [];
+                } else if (that.pickedPair.length === 2 && $(that.pickedPair[0]).data("image") === $(that.pickedPair[1]).data("image")) {
+
+                    $(that.pickedPair[0]).off("click");
+                    $(that.pickedPair[1]).off("click");
+                    that.pickedPair.length = 0;
                 }
 
             })
@@ -95,12 +142,11 @@ $(function () {
 
     }
 
-    let board = document.querySelector(".game-board");
-    let tilesDivs = document.querySelectorAll(".grid-item");
+    // let board = document.querySelector(".game-board");
+    // let tilesDivs = document.querySelectorAll(".grid-item");
 
-    let game = new Game(board, tilesDivs);
+    let game = new Game();
 
-    game.shuffleCards();
     game.newGame();
 
 })
