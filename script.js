0ec7de8c7b2e31b0da780a09f2af44462378a26d
infector <piotr.babik@gmail.com>
2018-06-27 15:23:35 -0700

    added raw score counter, fixed problem with change event firing twice

--- script.js
+++ script.js
@@ -2,14 +2,6 @@
 
 $(function () {
 
-    class Card {
-
-        constructor(div, image) {
-            this.div = div
-            this.image = image;
-        }
-    }
-
     class Game {
 
         constructor() {
@@ -45,10 +37,9 @@ $(function () {
         }
 
         newGame() {
-
             $(".game-board").empty();
             this.tilesDivs.length = 0;
-            $("select").prop("selectedIndex", -1);
+            $("select").show();
             this.createBoard();
         }
 
@@ -56,15 +47,18 @@ $(function () {
 
             let that = this;
 
-            $("select").change(function () {
-
+            // $("select").unbind("change"); this or the one below - uncomment (jQuery bug?)
+            $("select").change(function (e) {
+                // e.stopImmediatePropagation();
                 let element = '<div class="grid-item"></div>';
                 let createdDiv;
                 let difficulty = parseInt($("select").val());
                 let sideElemsCount = Math.sqrt(difficulty);
-                let sideElemSize = 80 / sideElemsCount + "vmin";
+                let sideElemSize = 60 / sideElemsCount + "vmin";
+                $(this).hide();
                 $(".game-board").css("grid-template-columns", `repeat(${sideElemsCount}, ${sideElemSize})`);
                 $(".game-board").css("grid-template-rows", `repeat(${sideElemsCount}, ${sideElemSize})`);
+                $("select").after("<h1>" + that.pairsClickedCounter + "</h1>");
 
                 for (let i = 0; i < difficulty; i++) {
                     $(".game-board").append(element);
@@ -72,13 +66,13 @@ $(function () {
                     that.tilesDivs.push(createdDiv);
                 }
 
-                that.addGameMechanics();
+                that.startGameMechanics();
                 that.shuffleCards();
             });
 
         }
 
-        addGameMechanics() {
+        startGameMechanics() {
 
             // let pickedPair = this.pickedPair;
             // let selectable = this.selectable;
@@ -105,16 +99,18 @@ $(function () {
                         $(that.pickedPair[1]).removeAttr("style");
                         that.pickedPair.length = 0;
                         that.selectable = true;
-
+                        that.pairsClickedCounter++;
+                        $("h1").text(that.pairsClickedCounter);
                     }, 500);
-
-                    console.log(that.pairsClickedCounter++);
+                    // console.log(that.pairsClickedCounter);
 
                 } else if (that.pickedPair.length === 2 && $(that.pickedPair[0]).data("image") === $(that.pickedPair[1]).data("image")) {
 
                     $(that.pickedPair[0]).off("click");
                     $(that.pickedPair[1]).off("click");
                     that.pickedPair.length = 0;
+                    that.pairsClickedCounter++;
+                    // console.log(that.pairsClickedCounter);
                 }
 
             })
@@ -122,10 +118,12 @@ $(function () {
         }
 
         shuffleCards() {
-
+            let that = this;
             let randElem;
             let temp;
-
+            $("button").click(function() {
+                that.newGame();
+                })
             for (let lastElemIndex = this.tilesDivs.length - 1; lastElemIndex >= 0; lastElemIndex--) {
                 randElem = Math.floor(Math.random() * lastElemIndex);
                 temp = this.tilesDivs[lastElemIndex];
